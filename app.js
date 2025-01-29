2cd97532d25052f424ea3941bf8c9c1a34c0cf16
Shash <ashishsinghlogic@gmail.com>
2025-01-29 09:52:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const filePath = process.argv[2];
 const fileContent = await readFile(filePath, "utf-8");
 
 const wordsArray = fileContent.split(/[\W]/).filter((w) => w);
-
+//here /[\w]/ will match any word character (alphanumeric & underscore) 
+//and /[\W]/ will match any non-word character (special characters)
+//and filter((w) => w) will remove empty strings from the array
 const wordsCount = {};
 
 wordsArray.forEach((word) => {
